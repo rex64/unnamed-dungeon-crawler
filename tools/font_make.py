7f011c45f8a9bcccada2bc1d9abfd6c4a6bb482a
rex64 <rex64@users.noreply.github.com>
2016-10-08 14:48:36 +0200

    bitmap font maker python script

--- tools/font_make.py
+++ tools/font_make.py
@@ -1,23 +1,18 @@
-import sys
-from PIL import Image, ImageFont, ImageDraw
+import os
+from PIL import Image
 
-print ('Number of arguments:', len(sys.argv), 'arguments.')
-print ('Argument List:', str(sys.argv))
+files = [];
 
+for filename in os.listdir("./font_standard"):
+    if filename.endswith(".bmp"):
+        files.append(filename)
 
-#im = Image.open("test.bmp")
-im = Image.new('RGB', (100, 100), "#ffffff")
+im = Image.new('RGB', (8 * len(files), 8), "#ffffff")
 
-draw = ImageDraw.Draw(im)
+for index, file in enumerate(files):
+    bitmap_char = Image.open("./font_standard/" + file)
+    im.paste(bitmap_char, (8 * index, 0))
 
-# # use a bitmap font
-# font = ImageFont.load("arial.pil")
-
-# draw.text((10, 10), "hello", font=font)
-
-# use a truetype font
-font = ImageFont.truetype("Early GameBoy.ttf", 15)
-
-draw.text((0, 0), "world", font=font, fill=128)
-
-im.show()
+# im.show()
+im.save("standard_font.bmp")
+print("ok")
