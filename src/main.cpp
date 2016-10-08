3a78ab58c9d083bd3aa9757c5fd6807a7e6737cc
rex64 <rex64@users.noreply.github.com>
2016-10-08 19:13:59 +0200

    meh console

--- src/main.cpp
+++ src/main.cpp
@@ -18,12 +18,25 @@
 #endif
 
 #include <string>
+#include <unordered_map>
+#include <iostream>
 
 #define SCREEN_WIDTH 400
 #define SCREEN_HEIGHT 225
 
 #define GAME_WIDTH 256
 #define GAME_HEIGHT 144
+
+struct Entity {
+	int pos;
+	std::string surfaceStr;
+};
+
+struct Console {
+	bool visible;
+	std::string cmd;
+	std::string buff;
+};
 
 //http://www.lua.org/manual/5.3/manual.html#lua_CFunction
 static int luaTestFunc(lua_State* state)
@@ -37,6 +50,20 @@ static int luaTestFunc(lua_State* state)
 
 	lua_pushnumber(state, 123);
 	return 1; /* number of results */
+}
+
+void renderTextLine(std::string str, int x, int y, SDL_Surface* charSet, SDL_Surface* surf) {
+
+	unsigned int i = 0;
+	for (char& c : str) {
+
+		unsigned int charIndex = c - 32;
+		SDL_Rect src = { 8 * charIndex, 0, 8, 8 };
+		SDL_Rect dst = { (8 + x) * i, y * 8 , 8, 8 };
+
+		SDL_BlitSurface(charSet, &src, surf, &dst);
+		++i;
+	}
 }
 
 void renderText(std::string str, SDL_Surface* charSet, SDL_Surface* surf) {
@@ -57,10 +84,13 @@ void renderText(std::string str, SDL_Surface* charSet, SDL_Surface* surf) {
 		SDL_BlitSurface(charSet, &src, surf, &dst);
 		++i;
 	}
-
 }
 
 int main(int argc, char* argv[]) {
+
+	Entity player = { 0 };
+	Console console = { false, "" };
+
 
     SDL_version compiled;
     SDL_version linked;
@@ -97,6 +127,9 @@ int main(int argc, char* argv[]) {
 //    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH*4, SCREEN_HEIGHT*4);
     
     //SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "ERROR", SDL_GetError(), window);
+
+	//RESOURCES
+	std::unordered_map<std::string, SDL_Surface*> spritesheets;
     
     SDL_Surface *screen = SDL_CreateRGBSurface(0, SCREEN_WIDTH, SCREEN_HEIGHT, 32, 0, 0, 0, 0);
     SDL_Surface *game   = SDL_CreateRGBSurface(0, GAME_WIDTH, GAME_HEIGHT, 32, 0, 0, 0, 0);
@@ -105,8 +138,14 @@ int main(int argc, char* argv[]) {
     SDL_Surface *borderSurface = SDL_LoadBMP("data/base/borders/border.bmp");
 	SDL_Surface *tile00Surface = SDL_LoadBMP("data/base/tiles/tile00.bmp");
 	SDL_Surface *tile01Surface = SDL_LoadBMP("data/base/tiles/tile01.bmp");
-	SDL_Surface *player = SDL_LoadBMP("data/base/spritesheets/player.bmp");
+	{
+		std::string file = "data/base/spritesheets/player.bmp";
+		spritesheets[file] = SDL_LoadBMP(file.c_str());
+		player.surfaceStr = file;
+	}
 	SDL_Surface *bmpFont = SDL_LoadBMP("data/base/fonts/standard_font.bmp");
+
+	
 
     bool quit = false;
 
@@ -122,6 +161,8 @@ int main(int argc, char* argv[]) {
 
 	luaL_dostring(state, "io.write(\"luaTestFunc\")");
 	luaL_dostring(state, "luaTestFunc(\"First\", \"Second\", 112233)");
+
+	luaL_dostring(state, "io.write(\"ciao\")");
 	
 
 	//Dungeon
@@ -139,63 +180,157 @@ int main(int argc, char* argv[]) {
 
 	int arrayWidth  = 16;
 	int arrayHeight = 9;
-
-	int playerPos = 0;
 	 
     while (!quit)
     {
 
         while (SDL_PollEvent(&e) != 0)
         {
-            
-            const Uint8 *state = SDL_GetKeyboardState(NULL);
+			if (e.type == SDL_QUIT)
+			{
+				quit = true;
+			}
 
-            
-            if (e.type == SDL_QUIT)
-            {
-                quit = true;
-            }
+			//Console
+			if (console.visible == true){
+			
+				if (e.type == SDL_KEYDOWN) {
 
-            if (state[SDL_SCANCODE_LEFT]) {
-                
-				//int oldPos = playerPos;
+					SDL_Keymod omar = SDL_GetModState();
 
-				int x = playerPos % arrayWidth;
-				int y = (playerPos / arrayWidth) % arrayHeight;
+					if (e.key.keysym.sym == SDLK_q) console.cmd = console.cmd + "q";
+					if (e.key.keysym.sym == SDLK_w) console.cmd = console.cmd + "w";
+					if (e.key.keysym.sym == SDLK_e) console.cmd = console.cmd + "e";
+					if (e.key.keysym.sym == SDLK_r) console.cmd = console.cmd + "r";
+					if (e.key.keysym.sym == SDLK_t) console.cmd = console.cmd + "t";
+					if (e.key.keysym.sym == SDLK_y) console.cmd = console.cmd + "y";
+					if (e.key.keysym.sym == SDLK_u) console.cmd = console.cmd + "u";
+					if (e.key.keysym.sym == SDLK_i) console.cmd = console.cmd + "i";
+					if (e.key.keysym.sym == SDLK_o) console.cmd = console.cmd + "o";
+					if (e.key.keysym.sym == SDLK_p) console.cmd = console.cmd + "p";
+					if (e.key.keysym.sym == SDLK_a) console.cmd = console.cmd + "a";
+					if (e.key.keysym.sym == SDLK_s) console.cmd = console.cmd + "s";
+					if (e.key.keysym.sym == SDLK_d) console.cmd = console.cmd + "d";
+					if (e.key.keysym.sym == SDLK_f) console.cmd = console.cmd + "f";
+					if (e.key.keysym.sym == SDLK_h) console.cmd = console.cmd + "h";
+					if (e.key.keysym.sym == SDLK_j) console.cmd = console.cmd + "j";
+					if (e.key.keysym.sym == SDLK_k) console.cmd = console.cmd + "k";
+					if (e.key.keysym.sym == SDLK_l) console.cmd = console.cmd + "l";
+					if (e.key.keysym.sym == SDLK_z) console.cmd = console.cmd + "z";
+					if (e.key.keysym.sym == SDLK_x) console.cmd = console.cmd + "x";
+					if (e.key.keysym.sym == SDLK_c) console.cmd = console.cmd + "c";
+					if (e.key.keysym.sym == SDLK_v) console.cmd = console.cmd + "v";
+					if (e.key.keysym.sym == SDLK_b) console.cmd = console.cmd + "b";
+					if (e.key.keysym.sym == SDLK_n) console.cmd = console.cmd + "n";
+					if (e.key.keysym.sym == SDLK_m) console.cmd = console.cmd + "m";
+					
+					if (e.key.keysym.sym == SDLK_PERIOD) console.cmd = console.cmd + ".";
+					if (e.key.keysym.sym == SDLK_SEMICOLON) console.cmd = console.cmd + ";";
+					
+					if (e.key.keysym.sym == SDLK_QUOTE) {
+						if (e.key.keysym.sym == SDLK_QUOTE && omar & KMOD_SHIFT) {
+							console.cmd = console.cmd + "\"";
+						}
+						else {
+							console.cmd = console.cmd + "'";
 
-				x -= 1;
+						}
+					}					
 
-				playerPos = x + arrayWidth*y;
-            } else
-            
-            if (state[SDL_SCANCODE_RIGHT]) {
-               
-				int x = playerPos % arrayWidth;
-				int y = (playerPos / arrayWidth) % arrayHeight;
+					if (e.key.keysym.sym == SDLK_9) {
+						if (e.key.keysym.sym == SDLK_9 && omar & KMOD_SHIFT) {
+							console.cmd = console.cmd + "(";
+						} else {
+							console.cmd = console.cmd + "9";
 
-				x += 1;
+						}
+					}
 
-				playerPos = x + arrayWidth*y;
-            } else
+					if (e.key.keysym.sym == SDLK_0) {
+						if (e.key.keysym.sym == SDLK_0 && omar & KMOD_SHIFT) { 
+							console.cmd = console.cmd + ")"; 
+						}
+						else {
+							console.cmd = console.cmd + "0";
+						}
+					}
+					if (e.key.keysym.sym == SDLK_SPACE) console.cmd = console.cmd + " ";
+					if (e.key.keysym.sym == SDLK_BACKSPACE) console.cmd = console.cmd.substr(0, console.cmd.length() - 1);
 
-			if (state[SDL_SCANCODE_UP]) {
+				}
 
-				int x = playerPos % arrayWidth;
-				int y = (playerPos / arrayWidth) % arrayHeight;
+				//printf("console: %s\n", console.cmd.c_str());
 
-				y -= 1;
+				if (e.type == SDL_KEYDOWN) {
+					if (e.key.keysym.sym == SDLK_BACKQUOTE) {
+						console.visible = !console.visible;
+					}
 
-				playerPos = x + arrayWidth*y;
-			} else
+					if (e.key.keysym.sym == SDLK_RETURN) {
+						luaL_dostring(state, console.cmd.c_str());
+						console.cmd = "";
+						
+					}
+				}
 
-			if (state[SDL_SCANCODE_DOWN]) {
+			}
+			else {
 
-				int x = playerPos % arrayWidth;
-				int y = (playerPos / arrayWidth) % arrayHeight;
+				const Uint8 *state = SDL_GetKeyboardState(NULL);
 
-				y += 1;
 
-				playerPos = x + arrayWidth*y;
+				
+
+				if (e.type == SDL_KEYDOWN) {
+					if (e.key.keysym.sym == SDLK_BACKQUOTE) {
+						console.visible = !console.visible;
+					}
+				}
+
+				if (state[SDL_SCANCODE_LEFT]) {
+
+					//int oldPos = playerPos;
+
+					int x = player.pos % arrayWidth;
+					int y = (player.pos / arrayWidth) % arrayHeight;
+
+					x -= 1;
+
+					player.pos = x + arrayWidth*y;
+				}
+				else
+
+					if (state[SDL_SCANCODE_RIGHT]) {
+
+						int x = player.pos % arrayWidth;
+						int y = (player.pos / arrayWidth) % arrayHeight;
+
+						x += 1;
+
+						player.pos = x + arrayWidth*y;
+					}
+					else
+
+						if (state[SDL_SCANCODE_UP]) {
+
+							int x = player.pos % arrayWidth;
+							int y = (player.pos / arrayWidth) % arrayHeight;
+
+							y -= 1;
+
+							player.pos = x + arrayWidth*y;
+						}
+						else
+
+							if (state[SDL_SCANCODE_DOWN]) {
+
+								int x = player.pos % arrayWidth;
+								int y = (player.pos / arrayWidth) % arrayHeight;
+
+								y += 1;
+
+								player.pos = x + arrayWidth*y;
+							}
 			}
             
         }
@@ -229,12 +364,12 @@ int main(int argc, char* argv[]) {
 		//render player
 		{
 
-			int x = playerPos % arrayWidth;
-			int y = (playerPos / arrayWidth) % arrayHeight;
+			int x = player.pos % arrayWidth;
+			int y = (player.pos / arrayWidth) % arrayHeight;
 
 			//Player-->Game
 			SDL_Rect pos = { x * 16, y * 16, 16, 16 };
-			SDL_BlitSurface(player, NULL, game, &pos);
+			SDL_BlitSurface(spritesheets[player.surfaceStr], NULL, game, &pos);
 		}
 
 
@@ -250,6 +385,19 @@ int main(int argc, char* argv[]) {
 		//Game-->Screen
 		SDL_Rect location = { 72,40,100,100 };
 		SDL_BlitSurface(game, 0, screen, &location);
+
+		//Console-->Screen
+		{
+			if (console.visible) {
+				SDL_Surface *consoleSurface = SDL_CreateRGBSurface(0, SCREEN_WIDTH, SCREEN_HEIGHT-16, 32, 0, 0, 0, 0);
+				SDL_FillRect(consoleSurface, NULL, SDL_MapRGB(screen->format, 255, 255, 255));
+
+				renderTextLine(">" + console.cmd, 0, 24, bmpFont, consoleSurface);
+
+				SDL_BlitSurface(consoleSurface, 0, screen, 0);
+				SDL_FreeSurface(consoleSurface);
+			}
+		}
 
         SDL_UpdateTexture(texture, NULL, screen->pixels, screen->pitch);
         SDL_RenderClear(renderer);
