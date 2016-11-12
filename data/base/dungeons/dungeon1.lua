dd852313640aac70e236165a837973261fd94649
rex64 <rex64@users.noreply.github.com>
2016-11-12 17:03:26 +0100

    だって　君のことが。。。好きなの

--- data/base/dungeons/dungeon1.lua
+++ data/base/dungeons/dungeon1.lua
@@ -17,33 +17,33 @@ data.dungeons['data.base.dungeons.dungeon1'] = {
 	    0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0,
 	    0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0,
 	    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	}
+    }
 
-	EntityType = {
+    EntityType = {
 
-		Player = 0,
-		Enemy = 1,
-		Other = 2,
+      Player = 0,
+      Enemy = 1,
+      Other = 2,
 
-	}
+    }
 
-	for i,v in ipairs(a) do 
-		if v == 0 then
-			field.setTile(i-1, t1, v)
-		else
-			field.setTile(i-1, t0, v)
-		end
-	end
+    for i,v in ipairs(a) do 
+      if v == 0 then
+        field.setTile(i-1, t1, v)
+      else
+        field.setTile(i-1, t0, v)
+      end
+    end
 
-	--Player
-	local playerID = field.addEntity(EntityType.Player, 'data.base.entities.player')
-	field.setEntityTile(playerID, 1 + 16)
+    --Player
+    local playerID = field.addEntity(EntityType.Player, 'data.base.entities.player')
+    field.setEntityTile(playerID, 1 + 16)
 
-	local stairsID = field.addEntity(EntityType.Other, 'data.base.entities.stairs')
-	field.setEntityTile(stairsID, 1 + 18)
+    local stairsID = field.addEntity(EntityType.Other, 'data.base.entities.stairs')
+    field.setEntityTile(stairsID, 1 + 18)
 
-	local enemyID = field.addEntity(EntityType.Enemy, 'data.base.entities.enemy')
-	field.setEntityTile(enemyID, 1 + 24)
-	  end
+    local enemyID = field.addEntity(EntityType.Enemy, 'data.base.entities.enemy')
+    field.setEntityTile(enemyID, 1 + 24)
+  end
   
 }
